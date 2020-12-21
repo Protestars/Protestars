651e735f538495dbf159db725e5a7f708b54e267
Protestars <76135351+Protestars@users.noreply.github.com>
2020-12-21 22:17:14 +0100

    aggiunta suoni

---
+++ sketch.js
@@ -0,0 +1,62 @@
+let audio=[];
+let j=1;
+
+function preload(){
+  // put preload code here
+  soundFormats('mp3','ogg');
+    for (i=1; i<4; i++) {
+    audio[i] = loadSound('suoni/audio' + i + '.mp3');
+  }
+}
+
+function setup() {
+  //createCanvas(windowWidth,windowHeight)
+
+}
+
+function draw() {
+  // put drawing code here
+  // setTimeout(suonaurla, 5000);
+
+}
+
+function mousePressed() {
+suonaurla();
+
+}
+
+function suonaurla(){
+
+audio[1].play();
+audio[1].onended(suonaurla2);
+}
+
+function suonaurla2() {
+  audio[2].play();
+  audio[2].onended(suonaurla3);
+}
+
+function suonaurla3() {
+  audio[3].play();
+  audio[3].onended(suonaurla);
+}
+
+
+
+//   // for (j=1; j<4; j++){
+//   //   if (j<4){
+//   //     urla[j].play();
+//   //   }
+//   // }
+//   // if (j===4){
+//   //   j=0;
+//   // }
+//
+//   //   if (j<4) {
+//   //   urla[j].play();
+//   //   j++;
+//   // } else if (j===4){
+//   //   j=0;
+//   // }
+//
+// }
