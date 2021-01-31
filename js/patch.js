1308b4f7e464b9c4898e2b6d77f14485e9ad30cf
Protestars <76135351+Protestars@users.noreply.github.com>
2021-01-31 14:21:11 +0100

    html2

--- js/patch.js
+++ js/patch.js
@@ -8112,7 +8112,7 @@ CABLES.exportedPatches || (CABLES.exportedPatches = {}), CABLES.exportedPatches.
       }]
     }, {
       name: "URL",
-      value: "https://protestars.github.io/Protestars/About_us.html"
+      value: "https://protestars.github.io/Protestars/about.html"
     }, {
       name: "Target Name",
       value: "_self"
